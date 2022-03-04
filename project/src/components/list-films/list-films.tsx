b524336fbf89fbbfecbdfc26752bf4a93df62df6
VitaliiKorolev <korolevva88@yabdex.ru>
2022-03-04 14:35:38 +0300

    Улучшение списка карточек с фильмами

--- project/src/components/list-films/list-films.tsx
+++ project/src/components/list-films/list-films.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Genres } from '../../types/const';
 import { Film } from '../../types/film';
 import SmallFilmCard from '../small-film-card/small-film-card';
@@ -10,6 +10,11 @@ type Props = {
 
 function ListFilms({ films, genre }: Props) {
   const [activeFilm, setActiveFilm] = useState(0);
+  const [showFilmsCount, setShowFilmsCount] = useState(8);
+  useEffect(() => {
+    setShowFilmsCount(8);
+  }, [genre]);
+
   // eslint-disable-next-line no-console
   console.log(activeFilm);
   const filtredFilmsByGenre =
@@ -17,19 +22,38 @@ function ListFilms({ films, genre }: Props) {
       ? films
       : films.filter((film) => film.genre === genre);
 
+  const filmList = filtredFilmsByGenre.slice(0, showFilmsCount);
+
+  const handleShowMoreCLick = () => {
+    setShowFilmsCount(showFilmsCount + 8);
+  };
+
   return (
-    <div className="catalog__films-list">
-      {filtredFilmsByGenre.map((film) => (
-        <SmallFilmCard
-          key={film.id}
-          name={film.name}
-          posterImage={film.posterImage}
-          id={film.id}
-          onActiveFilmsSet={setActiveFilm}
-          videoLink={film.videoLink}
-        />
-      ))}
-    </div>
+    <>
+      <div className="catalog__films-list">
+        {filmList.map((film) => (
+          <SmallFilmCard
+            key={film.id}
+            name={film.name}
+            posterImage={film.posterImage}
+            id={film.id}
+            onActiveFilmsSet={setActiveFilm}
+            videoLink={film.videoLink}
+          />
+        ))}
+      </div>
+      {!(filtredFilmsByGenre.length - showFilmsCount < 0) && (
+        <div className="catalog__more">
+          <button
+            className="catalog__button"
+            type="button"
+            onClick={handleShowMoreCLick}
+          >
+            Show more
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 export default ListFilms;
